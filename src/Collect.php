c00859506d02fe22a4c98a6d8de9b05397f8715b
Hapikc <136709525532hs@gmail.com>
2025-05-14 20:58:45 +0700

    add some things

--- src/Collect.php
+++ src/Collect.php
@@ -1,14 +1,98 @@
 <?php
 
-namespace Collect;
+namespace Hapick\Collect;
 
 class Collect
 {
-    private array $array = [];
 
-    public function __construct(array $array = [])
+    private array $collection = [];
+
+    public function __construct(array $array)
     {
-        $this->array = $array;
+        $this->collection = $array;
+    }
+
+    public function pluck(string $key): Collect
+    {
+        return new Collect(array_column($this->collection, $key));
+    }
+
+    public function groupBy(string $key): Collect
+    {
+        $result = [];
+        foreach ($this->collection as $item) {
+            $result[$item[$key]][] = $item;
+        }
+        return new Collect($result);
+    }
+
+    public function flip(): Collect
+    {
+        return new Collect(array_flip($this->collection));
+    }
+
+    public function diff(array $array): Collect
+    {
+        return new Collect(array_diff($this->collection, $array));
+    }
+
+    public function intersect(array $array): Collect
+    {
+        return new Collect(array_intersect($this->collection, $array));
+    }
+
+    public function each(callable $callback): Collect
+    {
+        foreach ($this->collection as $key => $item) {
+            $callback($item, $key);
+        }
+        return $this;
+    }
+
+    public function map(callable $callback): Collect
+    {
+        return new Collect(array_map($callback, $this->collection));
+    }
+
+    public function reject(callable $callback): Collect
+    {
+        return $this->filter(function($item) use ($callback) {
+            return !$callback($item);
+        });
+    }
+
+    public function chunk(int $size): Collect
+    {
+        return new Collect(array_chunk($this->collection, $size));
+    }
+
+    public function collapse(): Collect
+    {
+        $results = [];
+        foreach ($this->collection as $array) {
+            $results = array_merge($results, $array);
+        }
+        return new Collect($results);
+    }
+
+    public function combine(array $values): Collect
+    {
+        return new Collect(array_combine($this->collection, $values));
+    }
+
+    public function contains($value): bool
+    {
+        return in_array($value, $this->collection, true);
+    }
+
+    public function count(): int
+    {
+        return count($this->collection);
+    }
+
+    public function toArray(): array
+    {
+        return $this->collection;
     }
 
     public function get($key = null)
@@ -20,30 +104,6 @@ class Collect
     {
         return $this->array[array_key_first($this->array)];
     }
-
-    public function count(): int
-    {
-        return count($this->array);
-    }
-
-    public function toArray(): array
-    {
-        return $this->array;
-    }
-
-    public function map(callable $callback): Collect
-    {
-        return new self(array_map($callback, $this->array));
-    }
-
-    public function each(callable $callback, ...$args): Collect
-    {
-        foreach ($this->array as $key => $item) {
-            $callback($item, $key, ...$args);
-        }
-        return $this;
-    }
-
     public function push($value, $key = null): Collect
     {
         if (gettype($value) === 'array') {
